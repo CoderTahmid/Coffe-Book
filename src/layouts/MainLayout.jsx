2bf2d56d7f760adba2868255757ffaeaa033869f
CoderTahmid <tahmid.ibne@gmail.com>
2025-04-29 20:35:40 +0600

    some changes in Navbar done, navbar's design done fully

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
         <div>
             {/* Navbar */}
             <Navbar></Navbar>
-            {/* Dynamic section */}
+            <div className='min-h-[calc(100vh-232px)] py-12'>
+                {/* Dynamic section */}
+                <Outlet></Outlet>
+            </div>
             {/* Footer */}
             <Footer></Footer>
         </div>
