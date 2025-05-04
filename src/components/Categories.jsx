7bdfaa1b9172ea5a832547e44ec3b99b59d4381d
CoderTahmid <tahmid.ibne@gmail.com>
2025-05-04 13:13:36 +0600

    Card desings added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Categories = ({ categories }) => {
     return (
         <div role="tablist" className="tabs tabs-lift tabs-lg">
             {
-                categories.map(category => <Link key={category.category} to={`/category/${category.category}`} role="tab" className="tab">{category.category}</Link>)
+                categories.map(category =>
+                    <NavLink
+                        key={category.category}
+                        to={`/category/${category.category}`}
+                        role="tab"
+                        className={({ isActive }) => `tab text-2xl font-bold ${isActive ? "tab-active" : ""}`}>
+                        {category.category}
+                    </NavLink>)
             }
         </div>
     );
--- src/pages/Coffees.jsx
+++ src/pages/Coffees.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Card from '../components/Card';
 
 const Coffees = () => {
+    const data = useLoaderData();
     return (
         <div>
-            Coffees
+            <div className='grid gtid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12'>
+                {
+                    data.map(coffee => <Card key={coffee.id} coffee={coffee}></Card>)
+                }
+            </div>
         </div>
     );
 };
