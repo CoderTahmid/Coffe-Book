1ed83da69425e5014e2503414694411688338ad9
CoderTahmid <tahmid.ibne@gmail.com>
2025-05-10 17:10:00 +0600

    card showed on the dashboard according to the local storage

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Heading from '../components/Heading';
+import { getAllFavorites } from '../utils';
+import Card from '../components/Card';
 
 const Dashboard = () => {
+    const [coffees, setCoffees] = useState([]);
+    useEffect(() => {
+        const favorites = getAllFavorites();
+        setCoffees(favorites);
+    }, []);
     return (
         <div>
-            Dashboard
+            <Heading title="Welcome to dashboard" subtitle="Manage coffees that you have previously added as favourite. You can view or remove them from here"></Heading>
+            <div className='grid gtid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12'>
+                {
+                    coffees.map(coffee => <Card key={coffee.id} coffee={coffee}></Card>)
+                }
+            </div>
         </div>
     );
 };
